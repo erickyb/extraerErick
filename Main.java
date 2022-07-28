d2e8034cf7ab1a68c8c177f9c9e530acf444dc83
Erick Yarleque B <102404443+erickyb@users.noreply.github.com>
2022-07-28 14:10:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{ 
+    public static void main(String[] args)
+    {
+        String frase = "Demonstracion de ingenio x kilometro";	
+        System.out.println("La Frase:  "  + frase);
+        System.out.println("Mi Nombre: " + frase.substring(1,2) + frase.substring(7,8) + frase.substring(10,11) + frase.substring(9,10) + frase.substring(27,28));
+        System.out.println("De la frase saque mi NOMBRE yeee ");
+    }
+}
